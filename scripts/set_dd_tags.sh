539bbd6bb968eb385c2111bea9c7d74075e21e47
laurent fuentes <lfuentes@jahia.com>
2019-04-23 15:26:59 +0200

    first push

---
+++ scripts/set_dd_tags.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/bash -l
+#
+
+tmpfile=/tmp/set_dd_tags.tmp
+ddconffile=/etc/datadog-agent/datadog.yaml
+hn_metadata=/metadata_from_HOST
+
+source ${hn_metadata}
+
+{
+echo "api_key: $DATADOGAPIKEY"
+echo "hostname: $(awk '$1=="hostname:"{print $2}' $ddconffile)"
+echo "tags:"
+echo " - client:$(awk '$2~/^client/ {split($2,a,":"); print a[2]}' $ddconffile)"
+echo " - product:$(awk '$2~/^product/ {split($2,a,":"); print a[2]}' $ddconffile)"
+echo " - envname:$(awk '$2~/^envname/ {split($2,a,":"); print a[2]}' $ddconffile)"
+echo " - provide:${_PROVIDE}"
+echo " - version:${DX_VERSION}"
+echo " - role:${_ROLE}"
+echo " - envmode:${jahia_cfg_operatingMode}"
+echo " - availability-zone:${JEL_AVAILABILITYZONE}"
+echo " - region:${JEL_REGION}"
+echo " - cloud-provider:${JEL_CLOUDPROVIDER}"
+echo " - hardware_node:${JEL_HOST_WHERE}"
+echo " - hn_hostname:${JEL_HOST_HOSTNAME}"
+echo " - cluster_role:${JEL_ENV_ROLE}"
+} > $tmpfile
+
+md5_tmp=$(md5sum $tmpfile | awk '{print $1}')
+md5_dd=$(md5sum $ddconffile| awk '{print $1}')
+
+if [ "$md5_tmp" == "$md5_dd" ]; then
+    echo "Tags are up to date, exiting."
+else
+    echo "Change detected in tag(s), updating Datadog conf..."
+    cp -f $tmpfile $ddconffile
+    systemctl restart datadog-agent
+fi
+
---
+++ scripts/setupUser.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+user=$1
+pswd=$2
+
+unset resp;
+resp=$(mysql -u$user -p$pswd mysql --execute="SHOW COLUMNS FROM user")
+[ -z "$resp" ] && {
+        service mysql stop;
+        mysqld_safe --skip-grant-tables --user=mysql --pid-file=/var/lib/mysql/$(hostname).pid &
+        sleep 5
+        cmd="CREATE TEMPORARY TABLE tmptable SELECT * FROM user WHERE User = 'root'; UPDATE tmptable SET User = '$user' WHERE User = 'root'; DELETE FROM user WHERE User = '$user'; INSERT INTO user SELECT * FROM tmptable WHERE User = '$user'; DROP TABLE tmptable;"
+        mysql mysql --execute="$cmd"
+
+        version=$(mysql --version|awk '{ print $5 }'|awk -F\, '{ print $1 }')
+
+        if (( $(awk 'BEGIN {print ("'$version'" >= "'5.7'")}') )); then
+                cmd="UPDATE user SET authentication_string=PASSWORD('$pswd') WHERE user='$user';";
+        else
+                cmd="UPDATE user SET password=PASSWORD('$pswd') WHERE user='$user';";
+        fi
+
+        mysql mysql --execute="$cmd"
+        echo $resp
+        rm -f /var/lib/mysql/auto.cnf
+        service mysql restart
+} || { 
+        echo "[Info] User $user has the required access to the database." 
+}
