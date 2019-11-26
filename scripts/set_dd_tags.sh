e58f0d2a25456af33bbe4e9a2ac600cad76e580b
laurent fuentes <lfuentes@jahia.com>
2019-11-26 18:31:44 +0100

    fix strange problem with the regexp as root user

--- scripts/set_dd_tags.sh
+++ scripts/set_dd_tags.sh
@@ -9,7 +9,7 @@ source ${hn_metadata}
 
 {
 echo "api_key: $DATADOGAPIKEY"
-echo "hostname: $_ROLE."$(hostname | sed 's/^[a-Z]\+\([0-9]\+\).*/\1/' | tr [A-Z] [a-z])
+echo "hostname: $_ROLE."$(hostname | sed 's/^[[:alpha:]]\+\([[:digit:]]\+\).*/\1/' | tr [A-Z] [a-z])
 echo "logs_enabled: true"
 echo "logs_config:"
 echo "  processing_rules:"
