cb53278b9c327d7c511bf04f7f560207ba57d6da
adoux@jahia.com <adoux@jahia.com>
2020-12-11 17:00:58 -0500

    PAAS-1308: fix haproxy healthcheck path

--- scripts/haproxy-backend-healthcheck.sh
+++ scripts/haproxy-backend-healthcheck.sh
@@ -2,7 +2,7 @@
 
 source /.jelenv
 
-res=$(curl -s $3:$4/healthcheck?token=$jahia_cfg_healthcheck_token |jq .status -r)
+res=$(curl -s $3:$4/modules/healthcheck?token=$jahia_cfg_healthcheck_token |jq .status -r)
 if [ $res = "GREEN" ]; then
   exit 0
 fi
