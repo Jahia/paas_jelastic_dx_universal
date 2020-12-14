e4b38aa699dcbb7474aa11d31a283a1ac42c584d
adoux@jahia.com <adoux@jahia.com>
2020-12-14 10:24:08 -0500

    PAAS-1324: allow YELLOW for haproxy healthcheck

--- scripts/haproxy-backend-healthcheck.sh
+++ scripts/haproxy-backend-healthcheck.sh
@@ -3,7 +3,7 @@
 source /.jelenv
 
 res=$(curl -s $3:$4/modules/healthcheck?token=$jahia_cfg_healthcheck_token |jq .status -r)
-if [ $res = "GREEN" ]; then
+if [ "$res" = "GREEN" ] || [ "$res" = "YELLOW" ]; then
   exit 0
 fi
 exit 1
